cd62f2ffee67c980fb145fe48584551644398847
Igor Tomych <igor@tomych.com>
2013-02-07 20:42:42 +0200

    pods update

---
+++ Homemoney/Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Homemoney
+//
+//  Created by Igor Tomych on 2/7/13.
+//  Copyright (c) 2013 Igor Tomych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, "5.0"
+pod 'FlurrySDK'
+pod 'AFNetworking'
+pod 'Reachability'
+pod 'MBProgressHUD'
+pod 'TestFlightSDK'
+pod 'JASidePanels'
+pod 'SSPullToRefresh'
