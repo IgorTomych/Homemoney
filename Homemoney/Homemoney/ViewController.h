6e7f4dc159731aff084677516c438cc4b3e0f9d1
Igor Tomych <igor@tomych.com>
2013-02-07 19:05:52 +0200

    single view application

---
+++ Homemoney/Homemoney/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Homemoney
+//
+//  Created by Igor Tomych on 2/7/13.
+//  Copyright (c) 2013 Igor Tomych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
